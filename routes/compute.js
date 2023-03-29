bbf23594120b572e3fa8a73809e990ea46f79f5b
saibharathkondepati <97245134+saibharathkondepati@users.noreply.github.com>
2023-03-29 17:40:10 -0500

    commiting all

--- routes/compute.js
+++ routes/compute.js
@@ -8,21 +8,21 @@ router.get('/', function(req, res, next) {
     if(req.query.x == undefined){
 
     
-    x=Math.floor(Math.random()*20)
-    y=Math.floor(Math.cosh()*10)
-    let ab = Math.acosh(x);
-    let abc = Math.log1p(x);
-    let abcd = Math.tan(x);
-    let abcdef = Math.sqrt(x);
-    res.send('Math.acosh() applied for'+x +'is' +ab+'\n Math.log1p() applied for'+x +'is'+abc+'\n Math.tan() applied for'+x +'is'+abcd+'\n Math.sqrt() applied for'+x +'is'+abcdef)
+    x=Math.floor(Math.random()*18)
+    y=Math.floor(Math.cosh()*12)
+    let xyz = Math.fround(x);
+    let xyz1 = Math.random(x);
+    let xyz2 = Math.round(x);
+    let xyz3 = Math.sqrt(x);
+    res.send('Math.fround() applied for '+x +'is' +xyz+'\n Math.random() applied for'+x +'is'+xyz1+'\n Math.sqrt() applied for'+x +'is'+xyz2)
 }
 else{
     x=req.query.x;
-    let ab = Math.acosh(x);
-    let abc = Math.log1p(x);
-    let abcd = Math.tan(x);
-    let abcdef = Math.sqrt(x);
-    res.send('Math.acosh() applied for'+x +'is' +ab+'\n Math.log1p() applied for'+x +'is'+abc+'\n Math.tan() applied for'+x +'is'+abcd+'\n Math.sqrt() applied for'+x +'is'+abcdef)
+    let xyz = Math.fround(x);
+    let xyz1 = Math.log1p(x);
+    let xyz2 = Math.tan(x);
+    let xyz3 = Math.sqrt(x);
+    res.send('Math.fround() applied for '+x +'is' +xyz+'\n Math.random() applied for'+x +'is'+xyz1+'\n Math.sqrt() applied for'+x +'is'+xyz2)
 }
 });
 
